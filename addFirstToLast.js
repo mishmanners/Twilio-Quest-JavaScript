6bb9f8e14b56cdbdcb001ae5186822f7d2d6566e
MishManners <mishmanners@github.com>
2020-04-14 16:38:41 +1000

    add things

--- addFirstToLast.js
+++ addFirstToLast.js
@@ -1,7 +1,7 @@
 function addFirstToLast(things) {
   let firstAndLast = '';
 
-  if (allthethings.length > 0 ) {
+  if (things.length > 0 ) {
       firstAndLast = things[0] + things[things.length - 1]
   }
   return firstAndLast;
